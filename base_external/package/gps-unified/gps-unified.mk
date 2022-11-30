c33d45418cc0d76e7a127f749fda08e3bdccd751
TejaswiniLakshminarayan <tela4318@colorado.edu>
2022-11-30 04:35:50 +0000

    load and unload of UART driver module

--- base_external/package/gps-unified/gps-unified.mk
+++ base_external/package/gps-unified/gps-unified.mk
@@ -4,24 +4,30 @@
 # GPS_UNIFIED
 #
 ##############################################################
-
+$(eval $(kernel-module))
 #Reference to the gps userspace code(Ganesh's repo)
-GPS_UNIFIED_VERSION = 8ffb4a2a559c2922510385c4490d72a1008adccc
+GPS_UNIFIED_VERSION = c0b5db0b129aa7086037bfaa8e45542b6adb8e43
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 GPS_UNIFIED_SITE = git@github.com:cu-ecen-aeld/final-project-Ganeshkm10.git
 GPS_UNIFIED_SITE_METHOD = git
 GPS_UNIFIED_GIT_SUBMODULES = YES
+GPS_UNIFIED_MODULE_SUBDIRS = UART_Driver/
 
 define GPS_UNIFIED_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/Updated_Userspace_GPS clean
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/Updated_Userspace_GPS all
+
+	
 endef
 
 # Installing the gps_unified executable
 define GPS_UNIFIED_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/Updated_Userspace_GPS/gps_unified $(TARGET_DIR)/usr/bin
+
+	
 endef
+
 
 $(eval $(generic-package))
