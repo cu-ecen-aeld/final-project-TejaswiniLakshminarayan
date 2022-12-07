28065c452b9e363c49d6a396766d37536b479b01
TejaswiniLakshminarayan <tela4318@colorado.edu>
2022-12-07 21:11:35 +0000

    final update hash

--- base_external/package/accelerometer-i2c/accelerometer-i2c.mk
+++ base_external/package/accelerometer-i2c/accelerometer-i2c.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #Reference to the accelerometer userspace code(Pranav's repo)
-ACCELEROMETER_I2C_VERSION = 0595752ac2d49cd2c127f9376e98fa53c8a47cb6
+ACCELEROMETER_I2C_VERSION = ddc3bdd37fe21703644ffb02f2c80aab1ac5aa85
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -21,7 +21,7 @@ endef
 
 # Installing the i2c_userspace executable
 define ACCELEROMETER_I2C_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/accelerometer-i2c/i2c_userspace $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/accelerometer-i2c/aesd_test $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
